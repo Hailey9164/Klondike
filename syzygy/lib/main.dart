d55068c9ca0cca45f79a88f41e09d6a8c043a989
Hailey Campbell <Haileybee2004@gmail.com>
2026-08-29 14:53:28 -0700

    almost done

--- syzygy/lib/main.dart
+++ syzygy/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
+import 'klondike_game.dart';
 
+// Main entry point for the Klondike game
 void main() {
-  final game = FlameGame();
+  final game = KlondikeGame();
   runApp(GameWidget(game: game));
 }
+
