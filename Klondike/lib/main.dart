383601c0b1fd2838a09f8d16cfd79b0c3e418755
Hailey Campbell <Haileybee2004@gmail.com>
2026-08-29 17:09:40 -0700

    fixed some name issues

--- Klondike/lib/main.dart
+++ Klondike/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
+
 import 'klondike_game.dart';
 
-// Main entry point for the Klondike game
 void main() {
   final game = KlondikeGame();
   runApp(GameWidget(game: game));
 }
-
